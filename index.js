65c235201c309501216d0432a8340e0ec272c49b
Israe Moubayed <israetarek@yahoo.com>
2021-05-28 09:26:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,35 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById('time').value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
+let greeting = document.getElementById('greeting');
+
 /* Write your implementation of greet() */
+
+function greet(string){
+  let time = string.split(':');
+  const hour = parseInt(time[0]);
+  if(hour<12) {
+    return "Good Morning"
+  }
+  else if(hour>17) {
+    return "Good Evening"
+  }
+  else {
+    return "Good Afternoon"
+  }
+}
+
+
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message){
+  greeting.innerHTML = message;
+  console.log(message);
+}
